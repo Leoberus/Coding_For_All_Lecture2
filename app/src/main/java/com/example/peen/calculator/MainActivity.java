fd87e90536fc5693fc4f41f90a7dd0d5fcb8528a
leoberus <ptrp.srirak@gmail.com>
2017-06-30 10:52:18 +0700

    Finisg Calculator

--- app/src/main/java/com/example/peen/calculator/MainActivity.java
+++ app/src/main/java/com/example/peen/calculator/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.peen.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    protected void calculate(View v){
+        EditText num1 = (EditText)findViewById(R.id.num1);
+        EditText num2 = (EditText)findViewById(R.id.num2);
+        TextView sum = (TextView) findViewById(R.id.sum);
+
+        Integer firstnum,secondnumber,result;
+        firstnum = Integer.parseInt(num1.getText().toString());
+        secondnumber = Integer.parseInt(num2.getText().toString());
+
+        result = firstnum+secondnumber;
+        sum.setText(""+result);
+
+    }
+
 }
